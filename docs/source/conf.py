a44e82d9c9adf584bbf12e4b4fccee626e9f9f22
Yeojin Kim <55701228+YeojinKim220@users.noreply.github.com>
2024-11-04 15:12:20 -0500

    Refactor Sphinx documentation build process and remove unnecessary files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'spaceexpress_docs'
-copyright = '2024, Yeojin'
-author = 'Yeojin'
-release = '1.0.0.'
+project = 'SpaceExpress'
+copyright = '2024, Yeojin Kim'
+author = 'Yeojin Kim'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['nbsphinx', 'myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
